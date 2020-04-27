c4550be389bc2e1f3c0a9b71e43e2350c051a073
LizzieGuajardoMozo <56008263+LizzieGuajardoMozo@users.noreply.github.com>
2020-04-27 15:59:17 -0500

    Updates

---
+++ precache-manifest.49494a9c087c36f550ad5fed3cd17ec6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821da3f880732508af2aed2eb72932f7",
+    "url": "/todo-list-beliveo/index.html"
+  },
+  {
+    "revision": "5da35339789af51dbe1c",
+    "url": "/todo-list-beliveo/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "5e6bb0c6ec159f6f3aa2",
+    "url": "/todo-list-beliveo/static/css/main.ef17f0b1.chunk.css"
+  },
+  {
+    "revision": "5da35339789af51dbe1c",
+    "url": "/todo-list-beliveo/static/js/2.ebe1cbfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-beliveo/static/js/2.ebe1cbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6bb0c6ec159f6f3aa2",
+    "url": "/todo-list-beliveo/static/js/main.6769cf4a.chunk.js"
+  },
+  {
+    "revision": "da9b7b66d004b4dd04f4",
+    "url": "/todo-list-beliveo/static/js/runtime-main.bf088cf0.js"
+  },
+  {
+    "revision": "713d7b95ab10e7f0a4b17a814c5b3ed3",
+    "url": "/todo-list-beliveo/static/media/cat.713d7b95.svg"
+  }
+]);
