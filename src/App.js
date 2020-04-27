dbf9002b0d50bc643377e6415e3aabe8a415cf8c
LizzieGuajardoMozo <56008263+LizzieGuajardoMozo@users.noreply.github.com>
2020-04-27 04:25:34 -0500

    To-Do task & Done Tasks
    Sección de My-To do tasks y done tasks completa. Marcar y desmarcar las casillas hace que las tareas cambien de sección según corresponda

--- src/App.js
+++ src/App.js
@@ -1,29 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [undoneTasks,setNewUndoneTask] = useState([]);  
+  const [newTask,setNewTask] = useState("")
+  const [doneTasks,setDoneTask] = useState([]);
+
+  function completeTask(index){    
+    setDoneTask(doneTasks.concat([undoneTasks[index]]).sort());
+    var arrTemp = undoneTasks;
+    arrTemp.splice(index,1);
+    setNewUndoneTask(arrTemp); 
+  };
+
+  function markAsUndone(index){
+    setNewUndoneTask(undoneTasks.concat([doneTasks[index]]).sort());
+    var arrTemp = doneTasks;
+    arrTemp.splice(index,1);
+    setDoneTask(arrTemp);
+  };
+
+  function addTask(){
+    if(newTask.replace(/\s/g, '').length)
+    {
+      //Ordena alfabeticamente
+      setNewUndoneTask(undoneTasks.concat([newTask]).sort());      
+    }
+    else{
+      window.console.log("agregar toast")
+    }
+    setNewTask("");
+  };
+  
   return (
     <div className = "body ">
-      <div class="container">
+      <div className="container">
         <row>
-          <div class="col-12">
+
+          <div className="col-12">
+            <div className="card">
+              <span className="text-center">
+                <input id="newitem" type="text" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="Add a new Task"/> 
+                <button className="btn btn-primary" type="submit" onClick={addTask}>
+                  <i className="material-icons align-middle" aria-hidden="true"  >add_circle_outline</i>
+                </button>              
+              </span>
+            </div>
+          </div>
+
+          <div className="col-12">
             <div className="card" id="todo">
               <h5 className="card-header text-center">My To-Do tasks</h5>
               <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                {undoneTasks.map(
+                    (task,index) => (
+                      <div className="form-check">
+                        <i className="check material-icons align-middle" onClick={()=> {completeTask(index);}}> check_box_outline_blank </i>
+                        <label className="form-check-label" for="defaultCheck1">
+                          {task}
+                        </label>
+                      </div>
+                    )
+                  )
+                }               
               </div>
             </div>
           </div>
-          <div class="col-12">
+          
+          <div className="col-12">
             <div className="card" id="done">
               <h5 className="card-header text-center">Done tasks</h5>
               <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              {doneTasks.map(
+                    (task,index) => (
+                      <div className="form-check">
+                        <i className="check material-icons align-middle" onClick={()=> {markAsUndone(index)}}> check_box </i>
+                        <label className="form-check-label done" for="defaultCheck1">
+                          {task}
+                        </label>
+                      </div>
+                    )
+                  )
+                }                     
               </div>
             </div>
           </div>
+
         </row>  
       </div>    
     </div>
