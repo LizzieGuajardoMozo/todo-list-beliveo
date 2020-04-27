f3b5b0a2efbca7bbed496e68da82f58b84869d6e
LizzieGuajardoMozo <56008263+LizzieGuajardoMozo@users.noreply.github.com>
2020-04-27 06:51:53 -0500

    Local Storage
    Elementos de la lista To-Do y Done se guardan en local storage
    Correción a css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [undoneTasks,setNewUndoneTask] = useState([]);  
-  const [newTask,setNewTask] = useState("")
-  const [doneTasks,setDoneTask] = useState([]);
+
+  var  loadedUndone = localStorage.getItem("todo")  
+  var  loadedDone = localStorage.getItem("done")
+
+  if(!loadedUndone){
+      loadedUndone = []
+  }
+  else
+  {
+    loadedUndone = loadedUndone.split(",");
+  }
+
+  if(!loadedDone){
+    loadedDone = []
+  }
+  else
+  {
+    loadedDone = loadedDone.split(",");
+  }
+
+  useEffect(()=> {
+    saveInLocalStorage();
+  })
+
+  const [undoneTasks,setNewUndoneTask] = useState(loadedUndone);  
+  const [newTask,setNewTask] = useState("");
+  const [doneTasks,setDoneTask] = useState(loadedDone);
+
+
+  function saveInLocalStorage(){
+    localStorage.setItem("todo",undoneTasks);
+    localStorage.setItem("done",doneTasks);
+  }
 
   function completeTask(index){    
     setDoneTask(doneTasks.concat([undoneTasks[index]]).sort());
     var arrTemp = undoneTasks;
     arrTemp.splice(index,1);
     setNewUndoneTask(arrTemp); 
+    saveInLocalStorage();
   };
 
   function markAsUndone(index){
@@ -18,6 +49,7 @@ function App() {
     var arrTemp = doneTasks;
     arrTemp.splice(index,1);
     setDoneTask(arrTemp);
+    saveInLocalStorage();
   };
 
   function addTask(){
@@ -25,26 +57,29 @@ function App() {
     {
       //Ordena alfabeticamente
       setNewUndoneTask(undoneTasks.concat([newTask]).sort());      
+      saveInLocalStorage();
     }
     else{
       window.console.log("agregar toast")
     }
     setNewTask("");
+    
   };
   
   return (
     <div className = "body ">
       <div className="container">
-        <row>
-
+        <row> 
           <div className="col-12">
-            <div className="card">
-              <span className="text-center">
-                <input id="newitem" type="text" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="Add a new Task"/> 
-                <button className="btn btn-primary" type="submit" onClick={addTask}>
-                  <i className="material-icons align-middle" aria-hidden="true"  >add_circle_outline</i>
-                </button>              
-              </span>
+            <div className="card" id="new">   
+                <div className="input-group text-center">
+                  <input id="newitem" type="text" class="form-control" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="Add a new Task" onKeyPress={event => {if(event.key == "Enter"){addTask()}}}/>
+                  <div className="input-group-append">
+                    <button className="btn btn-primary" type="submit" onClick={addTask}>
+                      <i className="material-icons align-middle" aria-hidden="true" >add_circle_outline</i>
+                    </button>
+                  </div>
+                </div>
             </div>
           </div>
 
@@ -54,11 +89,17 @@ function App() {
               <div className="card-body">
                 {undoneTasks.map(
                     (task,index) => (
-                      <div className="form-check">
-                        <i className="check material-icons align-middle" onClick={()=> {completeTask(index);}}> check_box_outline_blank </i>
-                        <label className="form-check-label" for="defaultCheck1">
-                          {task}
-                        </label>
+                      <div class="input-group">
+                        <div className="task-row">
+                          <div class="input-group-prepend">
+                            <i className="check material-icons icon-blue" onClick={()=> {completeTask(index);}}> check_box_outline_blank </i>
+                          </div>
+                        </div>
+                        <div className="col-10 task-row">
+                          <label className="form-check-label">
+                            {task}
+                          </label> 
+                        </div>
                       </div>
                     )
                   )
@@ -73,12 +114,17 @@ function App() {
               <div className="card-body">
               {doneTasks.map(
                     (task,index) => (
-                      <div className="form-check">
-                        <i className="check material-icons align-middle" onClick={()=> {markAsUndone(index)}}> check_box </i>
-                        <label className="form-check-label done" for="defaultCheck1">
-                          {task}
-                        </label>
-                      </div>
+                      <div class="input-group">
+                        <div className="task-row">
+                          <div class="input-group-prepend">
+                          <i className="check material-icons align-middle icon-blue" onClick={()=> {markAsUndone(index)}}> check_box </i></div>
+                        </div>
+                        <div className="col-10 task-row">
+                          <label className="form-check-label done">
+                            {task}
+                          </label> 
+                        </div>
+                      </div>     
                     )
                   )
                 }                     
