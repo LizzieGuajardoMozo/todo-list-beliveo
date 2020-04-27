c97a3e27e1fad8f118ea149e6a0a5a793dfce72a
LizzieGuajardoMozo <56008263+LizzieGuajardoMozo@users.noreply.github.com>
2020-04-27 15:11:25 -0500

    Delete Item
    La habilidad de borrar un task ya sea en todo o en done.
    Alertas para la acción de borrar y al tratar de ingrear un nuevo task invalido. favicon del sitio.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import catIcon from './cat.svg';
 
 
 function App() {
+
+  document.title = "To Do List";
+
   var  loadedUndone = localStorage.getItem("todo")  
   var  loadedDone = localStorage.getItem("done")
 
@@ -39,13 +42,34 @@ function App() {
     localStorage.setItem("done",doneTasks);
   }
 
+  function deleteDone(index){
+    if(window.confirm("Are you sure you want to delete this task? '"+doneTasks[index] +"'. This action cannot be reversed"))
+    {
+      var arrTemp = doneTasks;
+      arrTemp.splice(index,1);
+      setDoneTask(arrTemp); 
+      setNewUndoneTask(undoneTasks);
+      setNewTask(newTask+" ");
+    }    
+  }
+
+  function deleteUndone(index){
+    if(window.confirm("Are you sure you want to delete this task? '"+undoneTasks[index] +"'. This action cannot be reversed"))
+    {
+      var arrTemp = undoneTasks;
+      arrTemp.splice(index,1);
+      setNewUndoneTask(arrTemp);
+      setDoneTask(doneTasks);
+      setNewTask(newTask+" ");
+    }
+  }
+
   function completeTask(index){    
     setDoneTask(doneTasks.concat([undoneTasks[index]]).sort());
     var arrTemp = undoneTasks;
     arrTemp.splice(index,1);
     setNewUndoneTask(arrTemp); 
     saveInLocalStorage();
-
   };
 
   function markAsUndone(index){
@@ -60,17 +84,17 @@ function App() {
     if(newTask.replace(/\s/g, '').length)
     {
       //Ordena alfabeticamente
-      setNewUndoneTask(undoneTasks.concat([newTask]).sort());      
+      setNewUndoneTask(undoneTasks.concat([newTask.replace(/^\s+/g, '')]).sort());      
       saveInLocalStorage();
     }
     else{
-      window.console.log("agregar toast")
+      alert("New Task cannot be empty space")
     }
     setNewTask("");
     
   };
   
-  return (
+  return (    
     <div className = "body ">
       <div className="container">
         <row> 
@@ -105,7 +129,7 @@ function App() {
                       <div className="input-group">
                         <div className="task-row">
                           <div className="input-group-append">
-                            <i className="check material-icons icon-blue" onClick={()=> {completeTask(index);}}> check_box_outline_blank </i>
+                            <i className="check material-icons align-middle icon-blue" onClick={()=> {completeTask(index)}}> check_box_outline_blank </i>
                           </div>
                         </div>
                         <div className="col-10 task-row">
@@ -113,7 +137,10 @@ function App() {
                             {task}
                           </label> 
                         </div>
-                      </div>
+                        <div className="input-group-append">
+                          <i className="trash material-icons align-middle" onClick={()=> {deleteUndone(index)}}> delete </i>
+                        </div>
+                      </div>                      
                     )
                   )
                 }               
@@ -136,12 +163,16 @@ function App() {
                         <div className="input-group">
                           <div className="task-row">
                             <div className="input-group-append">
-                            <i className="check material-icons align-middle icon-blue" onClick={()=> {markAsUndone(index)}}> check_box </i></div>
+                              <i className="check material-icons align-middle icon-blue" onClick={()=> {markAsUndone(index)}}> check_box </i>
+                            </div>
                           </div>
-                          <div className="col-10 task-row">
+                          <div className=" col-10 task-row">
                             <label className="form-check-label done">
                               {task}
                             </label> 
+                          </div>  
+                          <div className="input-group-append">
+                            <i className="trash material-icons align-middle" onClick={()=> {deleteDone(index)}}> delete </i>
                           </div>
                         </div>     
                       )
